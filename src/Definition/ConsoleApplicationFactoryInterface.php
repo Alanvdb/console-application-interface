e95a67d8c0522b19b30e20dd6a84cf8b9a036d39
alanvdb <alanvdb.coder@gmail.com>
2025-06-15 16:56:45 +0200

    add comments

--- src/Definition/ConsoleApplicationFactoryInterface.php
+++ src/Definition/ConsoleApplicationFactoryInterface.php
@@ -4,5 +4,11 @@ namespace AlanVdb\Console\Definition;
 
 interface ConsoleApplicationFactoryInterface
 {
+    /**
+     * @param string $name Console application name
+     * @param string $version Console application version
+     * @param string ...$commands Console application commands
+     * @throws \AlanVdb\Console\Exception\InvalidArgumentException If a provided command does not implement \Symfony\Component\Console\Command\SignalableCommandInterface
+     */
     public function createConsoleApplication(string $name, string $version, string ...$commands): ConsoleApplicationInterface;
 }
